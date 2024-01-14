ba58647749cbe9f98154129aadbdebd42e3b97b2
kighteW3 <kighte04@gmail.com>
2024-01-14 22:08:23 +0100

    Updated footer bad performance disclaimer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ import "../styles/Footer.css";
 
 export default function Footer() {
   return (<footer className="footer">
-    <h1>¡Es posible que la primera acción que se haga en la página tarde mucho tiempo en cargar debido a que el host del servidor tumba la página cada 30 min y en cuanto vuelve a recibir una petición tiene que reiniciar todo el servicio!</h1>
+    <h1>¡It is possible that the first action/http req will delay so much because of the free plan of the hosting that i am using which turns off the service after 30 minutes of inactity and reboot it when a new req is recived!</h1>
   </footer>);
 }
