36acf2183745b97dc594e9de6e5b0113e6c04a77
kighteW3 <kighte04@gmail.com>
2024-01-14 22:18:40 +0100

    Fixed written errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ import "../styles/Footer.css";
 
 export default function Footer() {
   return (<footer className="footer">
-    <h1>¡It is possible that the first action/http req will delay so much because of the free plan of the hosting that i am using which turns off the service after 30 minutes of inactity and reboot it when a new req is recived!</h1>
+    <h1>It is possible that the first action/http req will delay so much because of the free plan of the hosting that i am using which turns off the service after 30 minutes of inactivity and reboot it when a new req is recived!</h1>
   </footer>);
 }
